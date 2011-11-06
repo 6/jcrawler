1a429f01ee75a619da2cc46e2f0c3615a948faa6
Peter Graham <peterghm@gmail.com>
2011-11-06 18:38:23 -0500

    make "by_year" function more generic, prints overall stats

--- 2ch_analyzer.py
+++ 2ch_analyzer.py
@@ -13,23 +13,28 @@ def analyze(csv_file):
     messages.append({"name": name, "email": email, "year": year, "age": age, "replies": replies, "length": length})
   print "Done parsing CSV"
 
-  name_by_year(messages)
+  x_by_year(messages, "name")
+  x_by_year(messages, "email")
+  x_by_year(messages, "length")
 
-def name_by_year(messages):
-  name_year_totals = {}
+def x_by_year(messages, x):
+  x_year_totals = {}
   for m in messages:
-    if m["year"] not in name_year_totals:
-      name_year_totals[m["year"]] = {"count": 0, "with_name": 0}
-    if m["name"] == 1:
-      name_year_totals[m["year"]]["with_name"] += 1
-    name_year_totals[m["year"]]["count"] += 1
+    if m["year"] not in x_year_totals:
+      x_year_totals[m["year"]] = {"count": 0, "val": 0}
+    x_year_totals[m["year"]]["val"] += m[x]
+    x_year_totals[m["year"]]["count"] += 1
   
   data = []
-  for year in name_year_totals:
-    totals = name_year_totals[year]
-    data.append([year, float(totals["with_name"]) / totals["count"]])
+  total_x, total_count = 0, 0
+  for year in x_year_totals:
+    totals = x_year_totals[year]
+    data.append([year, float(totals["val"]) / totals["count"]])
+    total_x += totals["val"]
+    total_count += totals["count"]
   
-  write_csv("2ch_name_year.csv", ("year", "%name"), data)
+  print "{0} total={1}, avg={2}".format(x, total_x, float(total_x)/total_count)
+  write_csv("2ch_{0}_year.csv".format(x), ("year", x), data)
   
 def write_csv(fname, headers, list_of_lists):
   f = open(fname, 'wb')
